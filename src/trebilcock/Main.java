e28957699ab12365d441e162adf3b842c1270f4e
Brenton Trebilcock <tre15004@byui.edu>
2020-02-01 16:20:12 -0800

    testing how to do an undo

--- src/trebilcock/Main.java
+++ src/trebilcock/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	System.out.println("Hello, my name is Brenton.");
 	System.out.println("Hello Brenton, my name is Scott");
+	System.out.println("Testing how to do undo's");
     }
 }
